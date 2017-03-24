952ee11026d423d750de2fdcf5f86a7c048d7270
August Conwell <august.conwell@gmail.com>
2017-03-24 02:01:34 +0000

    Fixed matching and waiting

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -1,16 +1,6 @@
-/* Setting pairs for chat and rps
- * 
- * On page load, collect userName
- * Check for an open game (with on ref(openGame).on 'value' listener) 
- * If no open game
- *      create new game
- *      add game to open games list
- *      display waiting for opponent
- * If there is an open game
- *      add user to the game
- *      remove game from open games list
- *      initialize game and chat
- */
+/*globals firebase */
+/*eslint-env browser, jquery*/
+
 
 // Initialize Firebase
 // Initialize Firebase
@@ -27,51 +17,112 @@ var database = firebase.database();
 
 // App variables
 
-var userName = 'testuser 1';
+var userName;
 var openGameInterval;
 var stillListening = true;
+var gameKey;
+var opponent;
 
 // App functions
 
+
 // Display the waiting stuff
 function displayWaitingForOpponent() {
-    
+    $('#display').html('<h3>... waiting for your opponent ...<h3>');
 }
 
+// Returns the opponent's user name
+function getOppoName(user1, user2) {
+    if (user1 === userName) {
+        return user2;
+    }
+    return user1;
+}
+
+// Once match is set, start the game
 function initializeGame() {
     
 }
 
-// On the page load, do these things
+// Once match is set, initialize the chat
+function initializeChat() {
+    
+}
 
-// Check for an open game for 100 ms
-// If there is no open game
-openGameInterval = setTimeout(function() {
-    displayWaitingForOpponent();
-    console.log(userName);
-    database.ref().push({
-        user1: userName
-    }).then(function(snapshot){
-        database.ref('openGame').set({
-            gameKey: snapshot.key
+
+// Initialize page
+function initializePage() {
+    
+    console.log('happening');
+    var form = $('<form id="userName">');
+    var field = $('<input id="nameField" type="text">');
+    var button = $('<input id="submitName" type="submit">');
+    var display = $('#display');
+    form.append(field);
+    form.append(button);
+    display.html(form);
+
+    // After you submit your username, do these things
+    $('#userName').submit(function(e) {
+        
+        console.log('submitted');
+        e.preventDefault();
+        
+        // set the userName
+        userName = $('#nameField').val();
+        // Show waiting screen
+        displayWaitingForOpponent();
+        // Check for an open game for 500 ms
+        // If there is no open game
+        openGameInterval = setTimeout(function() {
+            console.log(userName);
+            database.ref().push({
+                user1: userName
+            }).then(function(snapshot){
+                database.ref('openGame').set({
+                    gameKey: snapshot.key
+                });
+                gameKey = snapshot.key;
+                console.log('current gk:', gameKey);
+                database.ref(gameKey).on('child_added', function(snapshot) {
+                    if (snapshot.val() !== userName) {
+                        opponent = snapshot.val();
+                        console.log(opponent);
+                    }
+                });
+            });
+            stillListening = false;
+        }, 500);
+        
+        // If there is an open game
+        database.ref('openGame').once('child_added', function(snapshot) {
+            // If the open game interval hasn't timed out (so, if there is an available game, only before the game is set up)
+            if (stillListening) {
+                // Prevent no open game option from running
+                clearInterval(openGameInterval);
+                // Get the unique identifier for this game
+                gameKey = snapshot.val();
+                // Add the second user;
+                database.ref(gameKey).update({
+                    user2: userName
+                }).then(function(){
+                    // Get the opponent's user name
+                    database.ref(gameKey).once('value', function(snapshot2) {
+                        var user1 = snapshot2.val().user1;
+                        var user2 = snapshot2.val().user2;
+                        opponent = getOppoName(user1, user2);
+                        console.log('op:', opponent);
+                    });
+                   });
+                initializeGame(snapshot.val());
+                initializeChat(snapshot.val());
+                database.ref('openGame').remove();
+            }
+        
         });
-        gameKey = snapShot.key;
-        console.log('first gk', gameKey);
     });
-    stillListening = false;
-}, 500);
+}
 
-// If there is an open game
-database.ref('openGame').once('child_added', function(snapshot) {
-    console.log('hey');
-    if (stillListening) {
-        clearInterval(openGameInterval);
-        gameKey = snapshot.val();
-        console.log('second gk', gameKey);
-        database.ref(gameKey).update({
-            user2: 'user2'
-        });
-        initializeGame(snapshot.key);
-        database.ref('openGame').remove();
-    }
-});
+// On the page load, do these things
+// Set up the page;
+initializePage();
